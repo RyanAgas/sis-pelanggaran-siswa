1f3c3ff18d944ad8af185e3e003afdbf1533c28d
itsRayenn <166465388+itsRayenn@users.noreply.github.com>
2026-01-17 17:15:29 +0800

    entry point

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+header("Content-Type: application/json");
+
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../config/auth.php';
+require_once __DIR__ . '/../routes/api.php';
