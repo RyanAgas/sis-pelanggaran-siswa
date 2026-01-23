e1daded4768c2fd6b376a59376115502c6af08be
itsRayenn <166465388+itsRayenn@users.noreply.github.com>
2026-01-23 16:27:56 +0800

    penyesuaian insert pelanggaran dengan guru

--- models/Pelanggaran.php
+++ models/Pelanggaran.php
@@ -13,4 +13,31 @@ class Pelanggaran {
     public function __construct($db) {
         $this->conn = $db;
     }
+
+    public function getAll()
+{
+    $query = "SELECT * FROM {$this->table}";
+    $stmt = $this->conn->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+public function create($data)
+{
+    $query = "INSERT INTO {$this->table}
+              (siswa_id, guru_id, jenis_pelanggaran, poin, tanggal)
+              VALUES (:siswa_id, :guru_id, :jenis_pelanggaran, :poin, :tanggal)";
+
+    $stmt = $this->conn->prepare($query);
+
+    return $stmt->execute([
+        ':siswa_id' => $data['siswa_id'],
+        ':guru_id' => $data['guru_id'],
+        ':jenis_pelanggaran' => $data['jenis_pelanggaran'],
+        ':poin' => $data['poin'],
+        ':tanggal' => $data['tanggal']
+    ]);
+}
+
+
 }
