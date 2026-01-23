7cd4abda851b4dccc27fff0fe7ccda11f1102da5
itsRayenn <166465388+itsRayenn@users.noreply.github.com>
2026-01-23 17:33:15 +0800

    penyesuaian fitur keamanan, login, dan logout

--- config/auth.php
+++ config/auth.php
@@ -22,3 +22,29 @@ function roleUser()
 {
     return $_SESSION['user']['role_name'] ?? null;
 }
+
+//untuk user login
+function requireLogin()
+{
+    if(!isLogin()) {
+        http_response_code(401);
+        echo json_encode([
+            "status" => "error",
+            "message" => "Silahkan login terlebih dahulu"
+        ]);
+        exit;
+    }
+}
+
+//untuk user logout
+function logout()
+{
+    session_unset();   
+    session_destroy(); 
+
+    echo json_encode([
+        "status" => "success",
+        "pesan" => "Logout berhasil"
+    ]);
+    exit;
+}
