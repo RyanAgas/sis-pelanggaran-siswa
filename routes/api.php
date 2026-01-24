bf0e03cfee4ad02a5682eaef60fd302c1ca341e5
itsRayenn <166465388+itsRayenn@users.noreply.github.com>
2026-01-24 15:26:59 +0800

    merapikan code dan mengganti requireLogin

--- routes/api.php
+++ routes/api.php
@@ -1,31 +1,34 @@
 <?php
+
 require_once __DIR__ . '/../controllers/AuthController.php';
 require_once __DIR__ . '/../controllers/PelanggaranController.php';
 require_once __DIR__ . '/../controllers/PembinaanController.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Ambil URI yang bersih
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $scriptName = $_SERVER['SCRIPT_NAME'];
 
 $uri = substr($requestUri, strlen($scriptName));
-$uri = $uri === '' ? '/' : $uri;
+$uri = $uri === '' ? '/' : rtrim($uri, '/');
 
-//login
+// auth
+
 if ($method === 'POST' && $uri === '/login') {
     $controller = new AuthController();
     $controller->login();
     exit;
-} 
+}
 
-//log out
 if ($method === 'POST' && $uri === '/logout') {
-    requireLogin(); // pastikan sudah login
-    logout();
+    $controller = new AuthController();
+    $controller->logout();
     exit;
 }
 
-//pelanggaran
+// pelanggaran
+
 if ($method === 'GET' && $uri === '/pelanggaran') {
     $controller = new PelanggaranController();
     $controller->index();
@@ -38,22 +41,21 @@ if ($method === 'POST' && $uri === '/pelanggaran') {
     exit;
 }
 
-// Update Pelanggaran
 if ($method === 'PUT' && preg_match('#^/pelanggaran/(\d+)$#', $uri, $matches)) {
     $controller = new PelanggaranController();
     $controller->update($matches[1]);
     exit;
 }
 
-// Delete pelanggaran
 if ($method === 'DELETE' && preg_match('#^/pelanggaran/(\d+)$#', $uri, $matches)) {
     $controller = new PelanggaranController();
     $controller->destroy($matches[1]);
     exit;
 }
 
-//pembinaan
-if ($method == 'GET' && $uri === '/pembinaan') {
+// pembinaan
+
+if ($method === 'GET' && $uri === '/pembinaan') {
     $controller = new PembinaanController();
     $controller->index();
     exit;
@@ -65,32 +67,33 @@ if ($method === 'POST' && $uri === '/pembinaan') {
     exit;
 }
 
-//update pembinaan
 if ($method === 'PUT' && preg_match('#^/pembinaan/(\d+)$#', $uri, $matches)) {
     $controller = new PembinaanController();
     $controller->update($matches[1]);
     exit;
 }
 
-//DELETE Pembinaan
-if ($method == 'DELETE' && preg_match('#^/pembinaan/(\d+)$#', $uri, $matches)) {
+if ($method === 'DELETE' && preg_match('#^/pembinaan/(\d+)$#', $uri, $matches)) {
     $controller = new PembinaanController();
     $controller->destroy($matches[1]);
+    exit;
 }
 
-//test api
+// testing api
+
 if ($method === 'GET' && $uri === '/ping') {
     echo json_encode([
         "status" => "OK",
         "message" => "API modul sudah berjalan"
     ]);
     exit;
+}
 
-//end point tidak ketemu
+// not found/ gagal
+
 http_response_code(404);
 echo json_encode([
     "status" => "error",
     "message" => "endpoint tidak ditemukan"
 ]);
 exit;
-}
